85a37ff5033995c4a222431c36c0e3c1f101c39c
Rezvanov Maksim <reeezvanov@gmail.com>
2025-01-21 13:38:00 +0300

    Locked translation for explosion

--- src/abtestbed/world/explosion.rs
+++ src/abtestbed/world/explosion.rs
@@ -127,6 +127,7 @@ fn spawn_explosion(
                 cell.center(),
                 RigidBody::Dynamic,
                 Sensor,
+                LockedAxes::TRANSLATION_LOCKED,
                 ActiveEvents::COLLISION_EVENTS,
                 Collider::cuboid(SIZE.x / 2.0, SIZE.y / 2.0),
             ));
